f39ac6deb5b242a124cfb700692e6e02c8610bd3
Jan Winkler <jan.winkler.84@gmail.com>
2016-10-04 12:39:06 +0200

    Source code documentation

--- include/gdaplanner/Expression.h
+++ include/gdaplanner/Expression.h
@@ -14,6 +14,7 @@
 namespace gdaplanner {
   class ValueBase {
   public:
+    /** Shared pointer to ValueBase class **/
     typedef std::shared_ptr<ValueBase> Ptr;
     
     ValueBase() {}
@@ -23,6 +24,7 @@ namespace gdaplanner {
   template<typename T>
     class Value : public ValueBase {
   public:
+    /** Shared pointer to Value class **/
     typedef std::shared_ptr<Value<T>> Ptr;
     
   private:
@@ -41,17 +43,26 @@ namespace gdaplanner {
     }
   };
   
+  /** \brief Container class for holding arbitrary first-order logic expressions
+      
+      The Expression class can contain:
+      
+      * Other Expressions (as a list structure)
+      * Atoms (currently everything defined in Expression::Type)
+      
+   */
   class Expression : public Printable {
   public:
+    /** Shared pointer to Expression class **/
     typedef std::shared_ptr<Expression> Ptr;
     
     typedef enum {
-      List = 0,
-      String = 1,
-      Double = 2,
-      Float = 3,
-      Integer = 4,
-      UnsignedInteger = 5
+      List = 0,            /**< A list, containing other Expression instances */
+      String = 1,          /**< A zero-terminated character string; equivalent to \code{.cpp}std::string\endcode */
+      Float = 2,           /**< A single-precision floating point number */
+      Double = 3,          /**< A double-precision floating point number */
+      Integer = 4,         /**< A signed integer */
+      UnsignedInteger = 5  /**< An unsigned integer */
     } Type;
     
   private:
@@ -61,44 +72,173 @@ namespace gdaplanner {
     
   protected:
   public:
+    /** \brief Default constructor
+	
+	Initialized the Expression instance as an Expression::List. */
     Expression();
+    /** \brief List constructor
+	
+	Initialized the Expression instance as an
+	Expression::List. All elements from vecExpressions will be
+	copiedinto the new instance as list elements.
+    
+	\param vecExpressions The elements to initialize the Expression instance with */
     Expression(std::vector<Expression> vecExpressions);
+    /** \brief String constructor
+	
+	Initialized the Expression instance as an
+	Expression::String. strExpression will be copied as the
+	content string.
+	
+	\param strExpression String character sequence to use as content for the Expression instance */
     Expression(std::string strExpression);
+    /** \brief Character array constructor
+	
+	Initialized the Expression instance as an
+	Expression::String. arrcExpression will be copied as the
+	content string. This is effectively mirroring the behavior of
+	the string constructor. The character sequence given in
+	arrcExpression must be zero-terminated. Failing to do so will
+	result in undefined behavior.
+	
+	\param arrcExpression Character sequence to use as content for the Expression instance. This will be transformed into a std::string */
     Expression(const char* arrcExpression);
+    /** \brief Double-precision floating point number constructor
+	
+	Initialized the Expression instance as an
+	Expression::Double. dExpression will be copied as the
+	content string.
+	
+	\param dExpression Double-precision floating point number to use as content for the Expression instance */
     explicit Expression(double dExpression);
+    /** \brief Single-precision floating point number constructor
+	
+	Initialized the Expression instance as an
+	Expression::Float. fExpression will be copied as the
+	content string.
+	
+	\param fExpression Single-precision floating point number to use as content for the Expression instance */
     explicit Expression(float fExpression);
+    /** \brief Signed integer constructor
+	
+	Initialized the Expression instance as an
+	Expression::Integer. nExpression will be copied as the
+	content string.
+	
+	\param nExpression Signed integer to use as content for the Expression instance */
     explicit Expression(int nExpression);
+    /** \brief Unsigned integer constructor
+	
+	Initialized the Expression instance as an
+	Expression::UnsignedInteger. unExpression will be copied as the
+	content string.
+	
+	\param unExpression Unsigned integer to use as content for the Expression instance */
     explicit Expression(unsigned int unExpression);
     
+    /** \brief Default destructor */
     ~Expression();
     
+    /** \brief Returns the Expression::Type of this instance 
+
+	\return Expression::Type of this instance */
     Type type() {
       return m_tpType;
     }
     
+    /** \brief Returns the Value stored in this instance
+	
+	If the Expression instance is of a type other than
+	Expression::List (e.g. it is an atom), this templated function
+	will return the value stored in the atom. The template
+	argument must match the stored value type. Refer to type() to
+	validate the instance's contained value type.
+	
+	If the Expression instance is of type Expression::List or if
+	the type of the contained Value doesn't match the template
+	parameter, this function returns the default constructor of
+	the template type.
+	
+	Usage example:
+	\code{.cpp}
+	Expression exValue("Hello World");
+	std::cout << exValue.get<std::string>() << std::endl; // Will print `"Hello World"`
+	std::cout << exValue.get<int>() << std::endl; // Will print `0`
+	\endcode
+	
+	\return Value contained in the Expression instance, or T's default constructor */
     template<typename T>
       T get() {
-      typename Value<T>::Ptr vlValue = std::dynamic_pointer_cast<Value<T>>(m_vbValue);
-      
-      if(vlValue) {
-	return vlValue->get();
-      } else {
-	return T();
+      if(m_tpType == List) {
+	typename Value<T>::Ptr vlValue = std::dynamic_pointer_cast<Value<T>>(m_vbValue);
+	
+	if(vlValue) {
+	  return vlValue->get();
+	}
       }
+      
+      return T();
     }
     
+    /** \brief Returns the Expression's list length
+	
+	If this Expression instance is of type Expression::List,
+	return its list length.
+	
+	\return Length of list stored in this Expression */
     unsigned int size() {
       return m_vecSubExpressions.size();
     }
     
+    /** \brief Access individual elements in the list
+	
+	If this Expression instance is of type Expression::List,
+	return its element at the position specified by unIndex.
+	
+	If the Expression is not of type Expression::List, the return
+	value is undefined.
+	
+	\param unIndex Index of the element to return */
     Expression operator[](unsigned int unIndex) {
       return m_vecSubExpressions[unIndex];
     }
     
+    /** \brief Return vector of this list's sub-expressions
+	
+	If this Expression instance is of type Expression::List,
+	return its sub-expressions in order.
+	
+	If the Expression is not of type Expression::List, the return
+	value is undefined.
+	
+	\return std::vector of Expression instances stored in this Expression instance */
     std::vector<Expression>& subExpressions() {
       return m_vecSubExpressions;
     }
     
+    /** \brief Return a subset of sub-expressions from this list
+	
+	If this Expression instance is of type Expression::List,
+	return a subset of its sub-expressions in order. The subset
+	starts at index unStart of the original set of sub-expressions
+	and has at most unLength elements. If either unStart or
+	unLength are greater than the elements available in the
+	original list of elements, less elements will be returned in
+	the subset.
+	
+	Usage example:
+	\code{.cpp}
+	Expression exList({"Hello", "World", 1, 2, 3, 4.0});
+	Expression exIntegers = exList.subSequence(2, 3); // Will contain the elements `{1, 2, 3}`.
+	\endcode
+	
+	If the Expression is not of type Expression::List, the return
+	value is undefined.
+	
+	\param unStart Index of the first element to include in the sub-set
+	\param unLength Desired length of the sub-et
+	
+	\return std::vector of Expression instances from the original list */
     std::vector<Expression> subSequence(unsigned int unStart, unsigned int unLength = 0) {
       std::vector<Expression> vecSequence;
       
@@ -113,6 +253,38 @@ namespace gdaplanner {
       return vecSequence;
     }
     
+    /** \brief Parametrize an expression
+	
+	If an expression contains variables (starting with an `?`, not
+	including the wildcard `?_`), this function will replace all
+	their occurrences in the tree of Expression instances, based
+	on the variable values given in mapParametrization.
+	
+	Usage example:
+	\code{.cpp}
+	Expression exPrototype = Expression::parseString("(an object ((type ?type) (color ?color) (size ?size)))");
+	Expression exInstance = exPrototype.parametrize({{"?type", "Cup"}, {"?color", "red"}});
+	
+	std::cout << exInstance << std::endl; // Will print `(an object ((type Cup) (color red) (size ?size)))`
+	\endcode
+	
+	If variables are defined in the Expression that are not
+	defined in the mapParametrization map, their original symbol
+	is returned. If variables are defined in the map that are not
+	part of the original Expression, their value is ignored.
+	
+	The replacement can be whole Expression instances:
+	\code{.cpp}
+	Expression exPrototype = Expression::parseString("(an action-sequence ?do))");
+	Expression exDo = Expression::parseString("((pick ?object) (place ?object ?location))");
+	Expression exInstance = exPrototype.parametrize({{"?do", exDo}});
+	
+	std::cout << exInstance << std::endl; // Will print `(an action-sequence ((pick ?object) (place ?object ?location)))
+	\endcode
+	
+	\param mapParametrization The set of variables to replace in the original Expression
+	
+	\return Expression instance obeying the above formulation */
     Expression parametrize(std::map<std::string, Expression> mapParametrization) {
       switch(m_tpType) {
       case String: {
@@ -144,6 +316,7 @@ namespace gdaplanner {
       return *this;
     }
     
+    /** \brief Specifies how this class is printed to output streams */
     virtual std::string toString() override {
       std::stringstream sts;
       
@@ -189,10 +362,24 @@ namespace gdaplanner {
       return sts.str();
     }
     
+    /** \brief Check if this instance is a wildcard
+	
+	Wildcards are first-order logic elements denoted as `?_`. This
+	function returns a boolean value denoting whether this
+	instance is such a wildcard.
+	
+	\return Boolean value denoting whether this instance is a wildcard */
     bool isWildcard() {
       return (m_tpType == String && *this == "?_");
     }
     
+    /** \brief Check if this list includes a wildcard
+	
+	If this Expression instance is of type Expression::List,
+	return a boolean value denoting whether it contains a wildcard
+	element.
+	
+	\return Boolean value denoting whether this list contains a wildcard */
     bool hasWildcard() {
       if(m_tpType == List) {
 	for(Expression exTest : m_vecSubExpressions) {
@@ -205,6 +392,14 @@ namespace gdaplanner {
       return false;
     }
     
+    /** \brief Check if this instance is a variable
+	
+	Variables are first-order logic elements starting with a `?`
+	(not including the wildcard element being `?_`). This function
+	returns a boolean value denoting whether this instance is such
+	a variable.
+	
+	\return Boolean value denoting whether this instance is a variable */
     bool isVariable() {
       if(!this->isWildcard()) {
 	if(this->type() == String) {
@@ -221,6 +416,9 @@ namespace gdaplanner {
       return false;
     }
     
+    /** \brief Check if this instance is of type Expression::List
+	
+	\return Boolean value denoting whether this instance is of type Expression::List */
     bool isListOf(Type tpType) {
       if(m_tpType == List) {
 	bool bAllOfType = true;
@@ -369,6 +567,44 @@ namespace gdaplanner {
       return mapResolution;
     }
     
+    /** \brief Resolves this instance against another instance
+	
+	When resolving, two things happen:
+	
+	* Two instances of type Expression are checked for structural equality
+	
+	* When structurally equal, all variables are resolved against their counterpart
+	
+	Usage example:
+	\code{.cpp}
+	Expression exPrototype = Expression::parseString("(place-object ?object ?location)");
+	
+	Expression exSituationA = Expression::parseString("(place-object Cup Table)");
+	std::map<std::string, Expression> mapResolutionA = exPrototype.resolve(exSituationA);
+	// mapResolutionA is now `{{"?object", "Cup"}, {"?location", "Table"}}`
+	
+	Expression exSituationB = Expression::parseString("(place-object (an object) Table)");
+	std::map<std::string, Expression> mapResolutionB = exPrototype.resolve(exSituationA);
+	// mapResolutionA is now `{{"?object", Expression<"(an object)">}, {"?location", "Table"}}`
+	\endcode
+	
+	Variables on both sides:
+	\code{.cpp}
+	Expression exProtoA = Expression::parseString("(predicate ?a 2)");
+	Expression exProtoB = Expression::parseString("(predicate 1 ?b)");
+	
+	std::map<std::string, Expression> mapResolutionA = exProtoA.resolve(exProtoB);
+	// mapResolutionA is now `{{"?a", 1}, {"?b", 2}}`
+	\endcode
+	
+	After calling the function, the parameter bResolved passed by
+	reference denotes whether the two terms are structurally
+	equivalent and could be resolved.
+	
+	\param exOther The Expression instance to resolve against
+	\param bResolved Boolean flag denoting whether the resolution was successful
+	
+	\return Map of variable to value bindings */
     std::map<std::string, Expression> resolve(Expression& exOther, bool& bResolved) {
       std::map<std::string, Expression> mapResolution;
       bResolved = false;
