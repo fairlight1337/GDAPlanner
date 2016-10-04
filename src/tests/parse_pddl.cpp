cb2cb2caf743e243804a7ce226bcfa7da5c0494e
Jan Winkler <jan.winkler.84@gmail.com>
2016-10-04 15:40:53 +0200

    Added datasets and test for testing PDDL parsing

---
+++ src/tests/parse_pddl.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <gdaplanner/GDAPlanner.h>
+
+
+using namespace gdaplanner;
+
+
+bool exists(std::string strFile) {
+  std::ifstream ifFile(strFile, std::ios::in);
+  
+  return ifFile.good();
+}
+
+
+int main(int argc, char** argv) {
+  int nReturnvalue = EXIT_FAILURE;
+  
+  std::vector<std::string> vecTestDataDirectories = {"elevator", "openstacks", "parcprinter", "pegsol", "scanalyzer"};
+  
+  bool bOK = true;
+  for(std::string strDirectory : vecTestDataDirectories) {
+    std::string strFullDir = "../data/test/" + strDirectory;
+    
+    unsigned int unIndex = 1;
+    bool bGoon = true;
+    
+    while(true) {
+      std::stringstream sts;
+      sts << strFullDir << "/p" << (unIndex < 10 ? "0" : "") << unIndex;
+      
+      std::string strDomain = sts.str() + "-domain.pddl";
+      std::string strProblem = sts.str() + ".pddl";
+      
+      if(exists(strDomain) && exists(strProblem)) {
+	GDAPlanner gdapPlanner;
+	
+	if(gdapPlanner.readContextFile<loaders::PDDL>(strDomain)) {
+	  contexts::Context::Ptr ctxContext = gdapPlanner.currentContext();
+	  
+	  if(ctxContext) {
+	    problems::Problem::Ptr prbProblem = gdapPlanner.readProblemFile<loaders::PDDL>(strProblem);
+	    
+	    if(prbProblem) {
+	      unIndex++;
+	    } else {
+	      bOK = false;
+	      break;
+	    }
+	  } else {
+	    bOK = false;
+	    break;
+	  }
+	} else {
+	  bOK = false;
+	  break;
+	}
+      } else {
+	break;
+      }
+    }
+  }
+  
+  if(bOK) {
+    nReturnvalue = EXIT_SUCCESS;
+  }
+  
+  return nReturnvalue;
+}
