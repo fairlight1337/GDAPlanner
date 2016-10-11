b284bf545d679c66a54b6c44d243f56557ee1d0e
Jan Winkler <jan.winkler.84@gmail.com>
2016-10-11 16:32:34 +0200

    Automatically make sense of numerical values and don't interpret everything as a string by default

--- src/gdaplanner/Expression.cpp
+++ src/gdaplanner/Expression.cpp
@@ -8,10 +8,12 @@ namespace gdaplanner {
   Expression::Expression(std::vector<Expression> vecExpressions) : m_tpType(List), m_vecSubExpressions(vecExpressions) {
   }
   
-  Expression::Expression(std::string strExpression) : m_tpType(String), m_vbValue(Value<std::string>::create(strExpression)) {
+  Expression::Expression(std::string strExpression) : m_tpType(String), m_vbValue(nullptr) {
+    this->parseAssertStringValue(strExpression);
   }
   
-  Expression::Expression(const char* arrcExpression) : m_tpType(String), m_vbValue(Value<std::string>::create(std::string(arrcExpression))) {
+  Expression::Expression(const char* arrcExpression) : m_tpType(String), m_vbValue(nullptr) {
+    this->parseAssertStringValue(std::string(arrcExpression));
   }
   
   Expression::Expression(double dExpression) : m_tpType(Double), m_vbValue(Value<double>::create(dExpression)) {
@@ -28,7 +30,70 @@ namespace gdaplanner {
   
   Expression::~Expression() {
   }
-
+  
+  bool Expression::parseAssertStringValue(std::string strValue) {
+    // Check for floating point first; we resort to double rather than
+    // float per default. If we don't detect a decimal dot, we use
+    // integer.
+    if(strValue.length() > 0 && strValue != "-") {
+      bool bNegative = (strValue[0] == '-');
+      bool bDecimalDot = false;
+      bool bThisIsAString = false;
+      
+      if(bNegative) {
+	strValue = strValue.substr(1);
+      }
+      
+      for(unsigned int unI = 0; unI < strValue.length(); ++unI) {
+	if(strValue[unI] == '.') {
+	  if(unI == 0) {
+	    // Numbers don't start with a dot. This is a string.
+	    bThisIsAString = true;
+	    break;
+	  } else {
+	    bDecimalDot = true;
+	  }
+	} else if(!isdigit(strValue[unI])) {
+	  // There is a non-digit character in here. This is a string,
+	  // not a number.
+	  bThisIsAString = true;
+	  break;
+	}
+      }
+      
+      if(!bThisIsAString) {
+	if(bDecimalDot) {
+	  try {
+	    m_vbValue = Value<double>::create((bNegative ? -1 : 1) * std::stod(strValue));
+	    m_tpType = Double;
+	  } catch(const std::invalid_argument& ia) {
+	    // Something went wrong during conversion; this might be a
+	    // string after all.
+	    m_vbValue = Value<std::string>::create(strValue);
+	    m_tpType = String;
+	  }
+	} else {
+	  try {
+	    m_vbValue = Value<int>::create((bNegative ? -1 : 1) * std::stoi(strValue));
+	    m_tpType = Integer;
+	  } catch(const std::invalid_argument& ia) {
+	    // Something went wrong during conversion; this might be a
+	    // string after all.
+	    m_vbValue = Value<std::string>::create(strValue);
+	    m_tpType = String;
+	  }
+	}
+      } else {
+	m_vbValue = Value<std::string>::create(strValue);
+	m_tpType = String;
+      }
+    } else {
+      // Zero length; this is a string.
+      m_vbValue = Value<std::string>::create(strValue);
+      m_tpType = String;
+    }
+  }
+  
   void Expression::getVarNames(std::vector<std::string> & varNames) const
   {
       if(isVariable())
